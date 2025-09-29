dbdfe09fc5163b323a443e51bbde239eabded45b
Mike-K-CODER <kalumichael59@gmail.com>
2025-09-29 23:19:14 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+      let calculation = localStorage.getItem('calculation') || '';
+
+      // Display the calculation when the page first loads.
+      displayCalculation();
+
+      function updateCalculation(value) {
+        calculation += value;
+
+        // Display the calculation on the page
+        displayCalculation();
+
+        localStorage.setItem('calculation', calculation);
+      }
+
+      function displayCalculation() {
+        document.querySelector('.js-calculation')
+          .innerHTML = calculation;
+      }
